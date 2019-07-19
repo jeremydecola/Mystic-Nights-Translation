09731893add220a9585f98dabea33016e0991aed
Jeremy De Cola-David <jeremydecola@gmail.com>
2019-07-19 00:27:21 -0400

    In-Game Text Finding Tool
    A Python Tool that searches data files for UTF-8 and EUC-KR text.

--- findText.py
+++ findText.py
@@ -3,7 +3,6 @@ import os
 
 text = False
 results = []
-#path = 'C:/Users/jerem/Desktop/MysticNights/@MYSTICNIGHTS.ISO/'
 path = input("Enter search path : ")
 target = input("Enter search target : ")
 b_euckor = target.encode('euc-kr')
@@ -13,7 +12,6 @@ for root, dirs, files in os.walk(path):
      for file in files:
         with open(os.path.join(root, file), "rb") as data:
             buffer = data.read()  # read content
-        #text = re.search(b'\x4d\x45\x4d\x4f\x52\x59\x20\x43\x41\x52\x44\x20\x73\x6c\x6f\x74\x20\x31', buffer, re.MULTILINE)
         text_kor = re.search(b_euckor, buffer, re.MULTILINE)
         text = re.search(b_utf8, buffer, re.MULTILINE)
         if text or text_kor:
